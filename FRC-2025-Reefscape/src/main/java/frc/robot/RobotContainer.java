aa3870eb1ddfff017950da30ba0c7a357f9e96f4
RJRobotics <156985990+Scitobor-FRC@users.noreply.github.com>
2025-03-12 15:45:15 -0400

    fix auto directions

--- FRC-2025-Reefscape/src/main/java/frc/robot/RobotContainer.java
+++ FRC-2025-Reefscape/src/main/java/frc/robot/RobotContainer.java
@@ -164,13 +164,13 @@ public class RobotContainer {
 
     //Drive Out
     m_progOneAuto.addCommands(
-        new DriveAutoCommand(m_driveController, -.5, -.5),
+        new DriveAutoCommand(m_driveController, .50, -50),
         new WaitCommand(2.0),
         new DriveAutoCommand(m_driveController, 0.00, 0.00));
 
     // Place Coral Straight
     m_progTwoAuto.addCommands(
-      new DriveAutoCommand(m_driveController, -.50, -.50),
+      new DriveAutoCommand(m_driveController, .50, .50),
       new WaitCommand(4),
       new DriveAutoCommand(m_driveController, 0, 0),
       new CoralAutoCommand(m_coral, 1.00, 1.00),
